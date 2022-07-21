87b2785e186d211c7a79e0b5dd142948e85fe2be
PactodeVidaChurch <iglpactovida@gmail.com>
2022-07-21 16:14:34 -0600

    D

--- app/views/login.php
+++ app/views/login.php
@@ -24,8 +24,9 @@
 
     <main>
         <!--Cuadros de iniciar sesion o registrarse-->
+        <h2 id="title">IGLESIA PACTO DE VIDA</h2><br>
         <div class="contenedor__principal">
-            <h2 id="title">IGLESIA PACTO DE VIDA</h2><br>
+            <br>
             <div class="contenedor__inferior">
                 <div class="contenedor__inferior-login">
                     <h3>Si tienes una cuenta:</h3>
