5e75604ff3ae82e94780fa93b4f956ddf10f5a17
PactodeVidaChurch <iglpactovida@gmail.com>
2022-07-21 13:53:43 -0600

    M

--- app/views/live.php
+++ app/views/live.php
@@ -3,22 +3,23 @@
 <body>
 
     <section id="page-hero" class="section section__inverse justify-content-center pt-6 pb-4 pb-lg-5 pb-xl-6">
-        <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+        <div class="bg bg-img position-absolute" data-bg-img=".img"><img src="../../web/img/upcoming_events.jpg"
+                class="img" alt="Image" /></div>
         <!-- /.bg bg-img position-absolute -->
 
         <div class="container-xl position-relative section-content text-center py-4">
-            <h2 class="display-5 text-white mt-3 mt-lg-4">Proximos en vivos</h2>
+            <h2 class="display-5 text-white mt-3 mt-lg-4">En vivo</h2>
 
-            <p class="lead mb-4 mb-lg-5">Únete y comparte con nosotros!</p>
+            <strong><p class="lead mb-4 mb-lg-5">Únete y comparte con nosotros en este momento!</p></strong>
             <!-- /.section-filter -->
         </div>
         <!-- /.container-xl position-relative -->
     </section>
     <!-- #page-hero.section section__inverse justify-content-center pt-6 pb-4 pb-lg-5 pb-xl-6 -->
 
-    <section id="lives" class="section bg-ligth ">
+    <section id="lives" class="section">
        
-        <div class="row cities g-3 mb-4">
+        <div class="container row bg-black g-3 mb-4">
             <iframe width="700" height="700" src="https://www.youtube.com/embed/Lo3RbQw1ics"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
