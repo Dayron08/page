8f0094a655690e22f4b839c1bb79b92390c67362
PactodeVidaChurch <iglpactovida@gmail.com>
2022-07-21 14:01:45 -0600

    D

--- app/views/live.php
+++ app/views/live.php
@@ -10,7 +10,9 @@
         <div class="container-xl position-relative section-content text-center py-4">
             <h2 class="display-5 text-white mt-3 mt-lg-4">En vivo</h2>
 
-            <strong><p class="lead mb-4 mb-lg-5">Únete y comparte con nosotros en este momento!</p></strong>
+            <strong>
+                <p class="lead mb-4 mb-lg-5">Únete y comparte con nosotros en este momento!</p>
+            </strong>
             <!-- /.section-filter -->
         </div>
         <!-- /.container-xl position-relative -->
@@ -18,12 +20,12 @@
     <!-- #page-hero.section section__inverse justify-content-center pt-6 pb-4 pb-lg-5 pb-xl-6 -->
 
     <section id="lives" class="section">
-       
+
         <div class="container row bg-black g-3 mb-4">
             <iframe width="700" height="700" src="https://www.youtube.com/embed/Lo3RbQw1ics"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
         </div>
 
     </section>
