350b64c81cb4887c79ca7b2251e8b0373730615a
PactodeVidaChurch <iglpactovida@gmail.com>
2022-07-25 14:37:25 -0600

    M

--- Views/Includes/header.php
+++ Views/Includes/header.php
@@ -65,9 +65,9 @@
                                 data-bs-toggle="dropdown">Sobre Nosotros</a>
                             <ul class="nav-dropdown-menu dropdown-menu dropdown-menu-end shadow" data-bs-popper>
 
-                                <li><a class="dropdown-item" href="../Perfil/User/about_us.php">Iglesia</a></li>
+                                <li><a class="dropdown-item" href="../User/about_us.php">Iglesia</a></li>
 
-                                <li><a class="dropdown-item" href="../Perfil/User/testimonials.php">Testimonios</a></li>
+                                <li><a class="dropdown-item" href="../User/testimonials.php">Testimonios</a></li>
 
                             </ul>
                         </li>
@@ -79,9 +79,9 @@
                                     <span class="dropdown-header text-uppercase fw-bold">Galer&iacute;a</span>
                                 </li>
 
-                                <li><a class="dropdown-item" href="../Perfil/User/videos.php">Videos</a></li>
+                                <li><a class="dropdown-item" href="../User/videos.php">Videos</a></li>
 
-                                <li><a class="dropdown-item" href="../Perfil/User/images.php">Im&aacute;genes</a></li>
+                                <li><a class="dropdown-item" href="../User/images.php">Im&aacute;genes</a></li>
 
                                 <li>
                                     <hr class="dropdown-divider" />
@@ -95,12 +95,12 @@
 
                                 <li>
                                     <span class="dropdown-header text-uppercase fw-bold"><a class=""
-                                            href="../Perfil/User/events.php">Eventos</a></span>
+                                            href="../User/events.php">Eventos</a></span>
                                 </li>
 
                             </ul>
                         </li>
-                        <li><a class="nav-link" href="../Perfil/User/contact.php">Cont&aacute;ctenos</a></li>
+                        <li><a class="nav-link" href="../User/contact.php">Cont&aacute;ctenos</a></li>
 
                     </ul>
                     <!-- /.navbar-nav -->
