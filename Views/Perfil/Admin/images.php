f4400fc17745a6980217c12e03e7e05274fb0a51
PactodeVidaChurch <iglpactovida@gmail.com>
2022-07-27 14:50:48 -0600

    DJ

---
+++ Views/Perfil/Admin/images.php
@@ -0,0 +1,239 @@
+<?php require_once("../../Includes/header.php");?>
+<section id="page-hero"
+    class="section section__inverse justify-content-center py-5 py-lg-6 mb-n5 vh-50 h-lg-480px min-h-330px">
+    <div class="bg bg-img position-absolute" data-bg-img=".img">
+        <img loading="lazy" src="../../img/image_wallpaper.jpg" class="img" alt="Image" />
+
+        <div class="overlay bg-dark position-absolute o-50"></div>
+    </div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative section-content text-center py-4">
+        <h2 class="display-5 text-white mt-3 mt-lg-4">Imagenes Relacionadas</h2>
+
+        <p class="lead mb-4 mb-lg-5">Iglesia Pacto de Vida</p>
+    </div>
+    <!-- /.container-xl position-relative -->
+</section>
+
+<!-- #page-hero.section section__inverse justify-content-center py-5 py-lg-6 mb-n5 vh-50 h-lg-480px min-h-330px -->
+<section id="page-hero" class="section section__inverse justify-content-center pt-6 pb-4 pb-lg-5 pb-xl-6">
+
+
+    <div class="container-xl position-relative section-content text-center py-4">
+
+        <div class="section-filter pt-4">
+            <div class="row g-3">
+                <div class="col-12 col-md section-content text-center text-md-start">
+                    <p class="mb-0" style="color: black;"><strong>80</strong>&nbsp;Results&nbsp;Found</p>
+                </div>
+                <!-- /.col-12 col-md -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.section-filter -->
+    </div>
+    <!-- /.container-xl position-relative -->
+</section>
+<!-- #page-hero.section section__inverse justify-content-center pt-6 pb-4 pb-lg-5 pb-xl-6 -->
+<section id="news-1" class="section pb-6 mt-n4 mt-lg-n5 mt-xl-n6">
+    <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative">
+
+        <div class="row g-3 mb-5">
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/church/image_01.jpg" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/church/image_02.JPG" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+        </div>
+        <!-- /.row g-4 mb-5 -->
+
+        <footer class="section-footer d-flex justify-content-center">
+            <div class="btn-toolbar mb-0 me-3" role="toolbar">
+                <div class="btn-group me-2" role="group">
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Anterior</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm active px-3">1</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">2</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">3</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">4</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">5</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Siguiente</a>
+                </div>
+            </div>
+        </footer>
+        <!-- /.section-footer d-flex justify-content-center -->
+    </div>
+    <!-- /.container-xl -->
+</section>
+<!-- #news-1.section pb-6 mt-n4 mt-lg-n5 mt-xl-n6 -->
+
+<section id="cities-grid-2" class="section py-5 py-lg-6">
+    <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative h-100">
+        <header class="section-header mb-0">
+            <h2 class="fs-3 text-center section-title mb-3">Nuestros J&oacute;venes</h2>
+
+            <p class="lead text-center mb-0 o-75">Imag&eacute;nes relacionadas
+            </p>
+        </header>
+        <!-- /.section-header mb-5 -->
+        <div class="row g-3">
+            <div class="col-12 col-md section-content text-center text-md-start">
+                <p class="mb-0" style="color: black;"><strong>80</strong>&nbsp;Results&nbsp;Found</p>
+            </div>
+            <!-- /.col-12 col-md -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-xl -->
+</section>
+<section id="news-1" class="section pb-4 mt-n4 mt-lg-n5 mt-xl-n6">
+    <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative">
+
+        <div class="row g-3 mb-5">
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/young_people/image_01.jpg" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/young_people/image_02.jpg" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+        </div>
+        <!-- /.row g-4 mb-5 -->
+
+        <footer class="section-footer d-flex justify-content-center">
+            <div class="btn-toolbar mb-0 me-3" role="toolbar">
+                <div class="btn-group me-2" role="group">
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Anterior</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm active px-3">1</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">2</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">3</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">4</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">5</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Siguiente</a>
+                </div>
+            </div>
+        </footer>
+        <!-- /.section-footer d-flex justify-content-center -->
+    </div>
+    <!-- /.container-xl -->
+</section>
+<!-- #news-1.section pb-6 mt-n4 mt-lg-n5 mt-xl-n6 -->
+
+<section id="cities-grid-2" class="section py-5 py-lg-6">
+    <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative h-100">
+        <header class="section-header mb-2">
+            <h2 class="fs-3 text-center section-title mb-3">Nuestros ni&ntilde;os</h2>
+
+            <p class="lead text-center mb-0 o-75">Imag&eacute;nes relacionadas
+            </p>
+        </header>
+        <!-- /.section-header mb-5 -->
+        <div class="row g-3">
+            <div class="col-12 col-md section-content text-center text-md-start">
+                <p class="mb-0" style="color: black;"><strong>80</strong>&nbsp;Results&nbsp;Found</p>
+            </div>
+            <!-- /.col-12 col-md -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-xl -->
+</section>
+<section id="news-1" class="section pb-4 mt-n4 mt-lg-n5 mt-xl-n6">
+    <div class="bg bg-img position-absolute" data-bg-img=".img"></div>
+    <!-- /.bg bg-img position-absolute -->
+
+    <div class="container-xl position-relative">
+
+        <div class="row g-3 mb-5">
+
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/children/image_02.jpg" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+            <div class="col-12 col-sm-6 col-md-3 d-flex">
+                <div class="card card__minimal h-100">
+                    <div class="card-img-top overflow-hidden position-relative">
+                        <img loading="lazy" src="../../img/children/image_03.jpg" class="w-100" alt="Image"
+                            style="width: 100%; height : 250px;" />
+                    </div>
+                </div>
+                <!-- /.card card__minimal -->
+            </div>
+            <!-- /.col-12 col-sm-6 col-md-3 d-flex -->
+
+        </div>
+        <!-- /.row g-4 mb-5 -->
+
+        <footer class="section-footer d-flex justify-content-center">
+            <div class="btn-toolbar mb-0 me-3" role="toolbar">
+                <div class="btn-group me-2" role="group">
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Anterior</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm active px-3">1</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">2</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">3</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">4</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">5</a>
+                    <a href="#" class="btn btn-outline-primary btn-default btn-sm px-3">Siguiente</a>
+                </div>
+            </div>
+        </footer>
+        <!-- /.section-footer d-flex justify-content-center -->
+    </div>
+    <!-- /.container-xl -->
+</section>
+<!-- #news-1.section pb-6 mt-n4 mt-lg-n5 mt-xl-n6 -->
+
+
+
+<?php require_once("../../Includes/footer.php");?>
