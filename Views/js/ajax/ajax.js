bce259684fd8ecc8e2555dc7576b9fbb82df0a5a
Josue <JOSUEFRANREDONDOGOMEZ@GMAIL.COM>
2022-08-09 11:48:37 -0600

    v

--- Views/js/ajax/ajax.js
+++ Views/js/ajax/ajax.js
@@ -1,62 +1,83 @@
 $(document).ready(function () {
 
+    // $("#btn_singUp").prop('disabled',true);
 
-    $("#btn_singUp").click(function (e) { 
+        $("#btn_singUp").click(function (e) { 
 
-        e.preventDefault();
-
-        
-        var txt_user_id = $("#txt_user_id").val();
-        if(txt_user_id == 0 ){
-        
-            alert("Debe ingresar la cedula");
-            return false;
-        }else if(txt_user_id.length != 9){
+            e.preventDefault();
     
-            alert("El formato de cedula no es correcto");
-            return false;
-        }
-
-        var txt_name = $("#txt_name").val();
-        if(txt_name==0){
-            alert("El nombre esta vacio");
-            return false;
-        }
-
-        var txt_surname = $("#txt_surname").val();
-        if(txt_surname==0){
-            alert("El apellido esta vacio");
-            return false;
-        }
-
-        var txt_password = $("#txt_password").val();
-        if(txt_password==0){
-            alert("La contraseña esta vacia");
-            return false;
-        }
-        
-
-        var txt_gmail = $("#txt_gmail").val();
-      
-
-        $.ajax({
-            url: "../../../Controllers/singUp.php",
-            method: "POST",
-            data : {txt_user_id : txt_user_id, txt_name : txt_name, txt_surname : txt_surname, txt_password : txt_password, txt_gmail : txt_gmail},
-            success: function(dataresponse, statustext, response){
-                if(statustext == "success"){
-                    console.log("exitosamente")
-                    
-                    
-                }
-            },
-            error: function(request, errorcode, errortext){
-                console.log("errorrrrrr")
+            var txt_user_id=$("txt_user_id").val();
+            if(txt_user_id == 0 ){
+                $("#alert_id").text("No se permiten campos vacios");
+                $("#alert_id").css({"background-color":"#F38B77","font-family": "Times New Roman', Times, serif;"})
+                return false;          
+            }else if(!isNaN(txt_user_id)) {
+                $("#alert_id").text("No se permiten letras");
+                $("#alert_id").css({"background-color":"#F38B77","font-family": "Times New Roman', Times, serif;"})
+                return false;
+            }else if(!txt_user_id.lenght==9){
+                $("#alert_id").text("Número de Cedula no valido");
+                $("#alert_id").css({"background-color":"#F38B77","font-family": "Times New Roman', Times, serif;"})
+                return false;
+            }else{
+                return true;
             }
-        }); 
+            
+            
+            // var txt_user_id = $("#txt_user_id").val();
+            // if(txt_user_id == 0 ){
+            
+            //     alert("Debe ingresar la cedula");
+            //     return false;
+            // }else if(txt_user_id.length != 9){
+        
+            //     alert("El formato de cedula no es correcto");
+            //     return false;
+            // }
+    
+            var txt_name = $("#txt_name").val();
+            if(txt_name==0){
+                alert("El nombre esta vacio");
+                return false;
+            }
+    
+            var txt_surname = $("#txt_surname").val();
+            if(txt_surname==0){
+                alert("El apellido esta vacio");
+                return false;
+            }
+    
+            var txt_password = $("#txt_password").val();
+            if(txt_password==0){
+                alert("La contraseña esta vacia");
+                return false;
+            }
+            
+    
+            var txt_gmail = $("#txt_gmail").val();
+          
+    
+            $.ajax({
+                url: "../../../Controllers/singUp.php",
+                method: "POST",
+                data : {txt_user_id : txt_user_id, txt_name : txt_name, txt_surname : txt_surname, txt_password : txt_password, txt_gmail : txt_gmail},
+                success: function(dataresponse, statustext, response){
+                    if(statustext == "success"){
+                        console.log("exitosamente")
+                        
+                        
+                    }
+                },
+                error: function(request, errorcode, errortext){
+                    console.log("errorrrrrr")
+                }
+            }); 
+    
+    
+        });
+    
+    
 
-
-    });
 
 
     
