b696dff370bbf5c516dfb84dbf9bc6920e26499b
PactodeVidaChurch <iglpactovida@gmail.com>
2022-08-17 12:54:15 -0600

    M

--- Views/js/ajax/call.js
+++ Views/js/ajax/call.js
@@ -89,7 +89,32 @@ $(document).ready(function(e){
    
        }); 
 
+        // lectura para todos los testimonios User
+        var url = "../../../Controllers/call_testimonials.php";
+        $.getJSON(url, function(dato){
 
+            $.each(dato, function(i, testimonials){
+                
+                var t = 
+                "<div class=\"col-12 col-sm-6 col-lg-4 d-flex align-items-stretch\">"+
+                "<figure class=\"card testimonial h-100 text-center bg-transparent mb-0\">"+
+                    "<blockquote class=\"blockquote card-body bg-white shadow-sm rounded d-flex flex-column px-lg-5\">"+
+                        "<p class=\"fs-1 text-secondary o-25 mb-0\"><i class=\"fas fa-quote-right\"></i></p>"+
+                        "<p class=\"fs-6\">"+testimonials.dsc+"</p>"+
+                        "<p class=\"text-secondary text-decoration-none fs-sm mt-auto mb-3 o-75\">&mdash;<span class=\"fw-bold\">-"+testimonials.name+"</span> "+testimonials.lastname+"</p>"+
+                        "<div class=\"img px-3 px-lg-4\">"+
+                            "<img loading=\"lazy\" src=\"http://assets.coolorize.com/avatar/256x256/ariel-lustre-368240.jpg\" class=\"img rounded-circle\" alt=\"Image\" />"+
+                        "</div>"+
+                    "</blockquote>"+
+                "</figure>"+
+            "</div>";
+                
+            
+                $(t).appendTo("#bodytestimonialUser");   
+                                                    
+            });
+
+        }); 
        
 
 
