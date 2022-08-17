7c068bb1a703cbf5998f14f9d19e10f547b3f3a4
PactodeVidaChurch <iglpactovida@gmail.com>
2022-08-16 20:38:41 -0600

    M

---
+++ Views/js/ajax/call.js
@@ -0,0 +1,98 @@
+$(document).ready(function(e){ 
+ 
+ 
+    // lectura todos las imagenes
+     var url = "../../../Controllers/call_images.php";
+     $.getJSON(url, function(datos){
+ 
+         $.each(datos, function(i, images){
+            
+            var tr = 
+                "<div class=\"swiper-slide w-330px h-330px pe-4 d-flex\">"+
+                    "<div class=\"card shadow-sm shadow-hover trans-base zoom-bg-img o-hover-all\">"+
+                        "<div class=\"card-img bg bg-img\" data-bg-img=\".img\">"+
+                            "<img alt=\"Image\" src=\"../../img/"+images.image+"\" />"+
+                            "<div class=\"overlay bg-dark position-absolute o-25 o-50-hover\"></div>"+
+                        "</div>"+ 
+                    "</div>"+
+                "</div>";
+
+            //     "<div class=\"swiper-slide w-330px h-330px pe-4 d-flex\">"+
+            //         "<div class=\"card shadow-sm shadow-hover trans-base zoom-bg-img o-hover-all\">"+
+            //             "<div class=\"card-img bg bg-img\" data-bg-img=\".img\">"+
+            //                 "<img class=\"img\" alt=\"Image\" src=\"../../img/"+images.image+"\" />"+
+            //                 "<div class=\"overlay bg-dark position-absolute o-25 o-50-hover\"></div>"+
+            //             "</div>"+ 
+            //         "</div>"+
+            //   "</div>";
+            
+           
+             $(tr).appendTo("#tbodyimg");   
+                                                  
+         });
+ 
+     }); 
+
+
+    //  // lectura testimonios en home 
+    //  var url = "../../../Controllers/call_testimonials.php";
+    //  $.getJSON(url, function(dato){
+ 
+    //      $.each(dato, function(i, testimonials){
+            
+    //         var t = 
+    //         "<div class=\"col-12 col-md d-flex align-items-stretch\">"+
+    //             "<figure class=\"card testimonial h-100 text-start bg-transparent mb-0\">"+
+    //                 "<blockquote class=\" cardsbg blockquote card-body shadow-sm rounded d-flex flex-column px-lg-5\">"+
+    //                     "<p class=\"fs-1 text-secondary o-25 mb-0\"><i class=\"fas fa-quote-right\"></i></p>"+
+    //                     "<p class=\"fs-6\">"+testimonials.dsc+"</p>"+
+    //                     "<p class=\"text-secondary text-decoration-none fs-sm mt-auto mb-3 o-75\">"+
+    //                         "&mdash;<span class=\"fw-bold\">"+testimonials.name+" </span> "+testimonials.lastname+"</p>"+
+    //                 "</blockquote>"+
+    //             "</figure>"+
+    //       "</div>";
+            
+           
+    //          $(t).appendTo("#tbodytestimonials");   
+                                                  
+    //      });
+ 
+    //  }); 
+
+
+       // lectura para todos los testimonios 
+       var url = "../../../Controllers/call_testimonials.php";
+       $.getJSON(url, function(dato){
+   
+           $.each(dato, function(i, testimonials){
+              
+              var t = 
+              "<div class=\"col-12 col-sm-6 col-lg-4 d-flex align-items-stretch\">"+
+                "<figure class=\"card testimonial h-100 text-center bg-transparent mb-0\">"+
+                    "<blockquote class=\"blockquote card-body bg-white shadow-sm rounded d-flex flex-column px-lg-5\">"+
+                        "<button class=\"btn btn-link p-0 ms-4 shadow-none\"><a id=\"buttondelete\" class=\"fa fa-trash fa-lg\" href=\"#\"></a></button>"+
+                        "<input type=\"checkbox\" id=\"check\" >"+
+                        "<p class=\"fs-1 text-secondary o-25 mb-0\"><i class=\"fas fa-quote-right\"></i></p>"+
+                        "<p class=\"fs-6\">"+testimonials.dsc+"</p>"+
+                       "<p class=\"text-secondary text-decoration-none fs-sm mt-auto mb-3 o-75\">&mdash;<span class=\"fw-bold\">-"+testimonials.name+"</span> "+testimonials.lastname+"</p>"+
+                        "<div class=\"img px-3 px-lg-4\">"+
+                            "<img loading=\"lazy\" src=\"http://assets.coolorize.com/avatar/256x256/ariel-lustre-368240.jpg\" class=\"img rounded-circle\" alt=\"Image\" />"+
+                        "</div>"+
+                    "</blockquote>"+
+                "</figure>"+
+            "</div>";
+              
+             
+               $(t).appendTo("#bodytestimonial");   
+                                                    
+           });
+   
+       }); 
+
+
+       
+
+
+ });
+
+ 
