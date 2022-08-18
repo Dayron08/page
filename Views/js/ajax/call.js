d94e7bd3d5444f942b12afcb0d5bb501c13dd0a9
PactodeVidaChurch <iglpactovida@gmail.com>
2022-08-17 20:09:51 -0600

    M

--- Views/js/ajax/call.js
+++ Views/js/ajax/call.js
@@ -147,7 +147,7 @@ $(document).ready(function(e){
 
         $("#btn_testimony").click(function (e) {
 
-            e.preventDefault();
+           
 
             var txt_testimony = $("#txt_testimony").val();
             var txt_person = "30528745";
