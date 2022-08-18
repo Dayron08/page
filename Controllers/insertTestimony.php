1539c5bc137927a02a15b01afcfd85cb82d334a7
PactodeVidaChurch <iglpactovida@gmail.com>
2022-08-17 20:13:29 -0600

    M

--- Controllers/insertTestimony.php
+++ Controllers/insertTestimony.php
@@ -5,7 +5,9 @@ $insert = new SingUp();
    
 if(isset($_REQUEST['txt_person'])){
    
-    
+    echo $_REQUEST["txt_testimony"];
+    echo $_REQUEST["txt_person"];
+
     $insert -> set_testimony($_REQUEST["txt_testimony"]);
     $insert -> set_user_id($_REQUEST["txt_person"]);
 
