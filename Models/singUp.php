a9a31e0f4983550c484456bbcc198b8da29acbf9
Dayron08 <dayron035@outlook.es>
2022-08-31 12:30:00 -0600

    D

--- Models/singUp.php
+++ Models/singUp.php
@@ -112,6 +112,16 @@ class SingUp extends Connection_Mysql {
         $this->set_person_registration_id = $set_person_registration_id;
     }
 
+	public function password_encryption($pass){
+		$password_ecrypted = password_hash($pass, PASSWORD_DEFAULT);
+		$this->password = $password_ecrypted;
+	}
+
+	// public function password_decryption($pass){
+	// 	$password_deciphered = password_verify($pass, $new );
+	// 	$this->password = $password_deciphered;
+	// }
+
 
 	private function execute($query){
 
