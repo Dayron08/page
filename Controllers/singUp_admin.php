676a69de6199102b2d82f63d51cd4b78e6f896d7
Dayron08 <dayron035@outlook.es>
2022-08-31 12:29:45 -0600

    D

--- Controllers/singUp_admin.php
+++ Controllers/singUp_admin.php
@@ -7,7 +7,7 @@ if(isset($_REQUEST['txt_user_id'])){
     $insert -> set_user_id($_REQUEST["txt_user_id"]);
     $insert -> set_name($_REQUEST["txt_name"]);
     $insert -> set_surname($_REQUEST["txt_surname"]);
-    $insert -> set_password($_REQUEST["txt_password"]);
+    $insert -> password_encryption($_REQUEST["txt_password"]);
     $insert -> set_gmail($_REQUEST["txt_gmail"]);
     $insert -> set_rol($_REQUEST["txt_rol"]);
 
