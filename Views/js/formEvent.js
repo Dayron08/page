e6dc84d426913a79e0d07e6023e41340e7094d03
Dayron08 <dayron035@outlook.es>
2022-09-01 16:34:20 -0600

    D

--- Views/js/formEvent.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() =>{
-    $("#btn_insert_event").mous(function (e) { 
-        e.preventDefault();
-        alert("estrooo");
-    });
-});
--- config/APP.php
+++
@@ -1,6 +0,0 @@
-<?php
-	const SERVERURL="http://localhost/PactodeVida/";
-
-	const COMPANY="Pacto de vida";
-
-	date_default_timezone_set("America/El_Salvador");
