fc7c06f194195605e87c3943d9937540e258bb99
Dayron08 <dayron035@outlook.es>
2022-09-05 17:42:39 -0600

    D

--- Models/singUp.php
+++ Models/singUp.php
@@ -380,30 +380,6 @@ class SingUp extends Connection_Mysql {
 		return $events ;
 
 	} 
-
-	public function getEvent() {
-		 
-		$this->query = "CALL P_VER_EVENTO(
-		'".$this->idEvent."');";
-		
-		$this->execute($this->query);
-
-	   $event = array();
-		while ($result = mysqli_fetch_assoc($this->result)) {
-			$event []= array(
-				"id"=> $result["ID_EVENT"],
-				"nameEvent"=> $result["NOMBRE_EVENTO"],
-				"name" => $result["ENCARGADO"],
-				"dsc" => $result["DSC_EVENTO"],
-				"img" => $result["IMG_PATH"],
-				"time" => $result["HORA_EVENTO"],
-				"date" => $result["FECHA_EVENTO"]);
-		}
-		
-		return $event ;
-
-	} 
-
 	public function getProfile() {
 		 
 		$this->query = "CALL P_VER_PERFIL(
@@ -451,32 +427,6 @@ class SingUp extends Connection_Mysql {
 		}
 		
 		return $event ;
-
-	} 
-
-	public function getProfile() {
-		 
-		$this->query = "CALL P_VER_PERFIL(
-		'".$this->user_id."');";
-
-		// $this->query = "SELECT * FROM `persona` WHERE `ID_REGISTRO_PERSONA` = '".$this->user_id."'";
-		
-		$this->execute($this->query);
-
-	   $user = array();
-		while ($result = mysqli_fetch_assoc($this->result)) {
-			$user []= array(
-				"id"=> $result["CEDULA"],
-				"rol"=> $result["DSC_TIPO"],
-				"name"=> $result["NOMBRE"],
-				"lastname" => $result["APPELLIDOS"],
-				"date" => $result["FECH_NACIMIENTO"],
-				"gender" => $result["GENERO"],
-				"email" => $result["EMAIL"],
-				"phone" => $result["TELEFONO"]);
-		}
-		
-		return $user ;
 
 	} 
 
